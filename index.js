26f458b3fdc59ee1e839bd3585cba13423401377
dlfedie <49173142+dlfedie@users.noreply.github.com>
2022-12-01 20:43:42 -0600

    so it begins

---
+++ index.js
@@ -0,0 +1,13 @@
+const input = require("./input");
+
+const day1 = () => {
+  console.log('woo');
+
+  let puzzleInput = input.elves.split('\n');
+
+  console.log(puzzleInput);
+
+
+}
+
+day1();
---
+++ input.js
@@ -0,0 +1,10 @@
+const elves = 
+`10
+20
+
+30
+
+40
+50`;
+
+module.exports = { elves };
