1fc1850b00a2e5319f13b8a9996493398b199021
dlfedie <49173142+dlfedie@users.noreply.github.com>
2022-12-04 18:16:44 -0600

    day 3 part 2 done

--- day3/index.js
+++ day3/index.js
@@ -17,7 +17,7 @@ const dayThreePartOne = () => {
       totalPriorities = totalPriorities + priority;
     }
 
-    console.log('totals: ', totalPriorities);
+    console.log('total of priorities: ', totalPriorities);
   }
 
   const compareCompartments = (rucksack) => {
@@ -53,6 +53,70 @@ const dayThreePartOne = () => {
   sumOfPriorities(mainInput);
 };
 
+const dayThreePartTwo = () => {
+
+  const mainInput = input.rucksacks.split('\n');
+
+  const sumOfPriorities = (puzzleInput) => {
+    let totalPriorities = 0;
+    
+    let firstInGroup = 0;
+    let lastInGroup = 3;
+
+    if ((puzzleInput.length % 3) !== 0) {
+      console.log('haha, find the right # of elves');
+      return 'haha, find the right # of elves';
+    }
+
+    let numberOfGroupsOfThreeElves = puzzleInput.length/3;
+    // console.log('# of groups: ', numberOfGroupsOfThreeElves);
+
+    for (let i = 0; i < numberOfGroupsOfThreeElves; i++) {
+      let groupOfElves = puzzleInput.slice(firstInGroup, lastInGroup);
+      let commonLetter = compareRucksacks(groupOfElves);
+      // console.log('commonletter: ', commonLetter);
+      let priority = calculatePriority(commonLetter);
+      totalPriorities = totalPriorities + priority;
+      firstInGroup = firstInGroup + 3;
+      lastInGroup = lastInGroup + 3;
+    }
+    
+    console.log('total of group priorities: ', totalPriorities);
+  }
+
+  const compareRucksacks = (rucksacks) => {
+    let commonLetter = '';
+    let firstRucksack = rucksacks[0];
+    let secondRucksack = rucksacks[1];
+    let thirdRucksack = rucksacks[2];
+    for (let letter of firstRucksack) {
+      if (secondRucksack.includes(letter) && thirdRucksack.includes(letter)) {
+        commonLetter = letter;
+        break;
+      }
+    }
+    
+
+    // console.log(commonLetter);
+    return commonLetter;
+  }
+
+  const calculatePriority = (letter) => {
+    // charCodeAt returns ascii value.. a = 97, A = 65. we want a = 1, and A = 27, etc.
+    let charCode = letter.charCodeAt(0);
+    if (charCode < 97) {
+      // console.log(charCode - 38);
+      return charCode - 38;
+    } else {
+      // console.log(charCode - 96);
+      return charCode - 96;
+    }
+
+  }
+
+  sumOfPriorities(mainInput);
+};
 
 
 dayThreePartOne();
+dayThreePartTwo();
