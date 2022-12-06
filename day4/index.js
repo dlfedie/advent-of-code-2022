148efaaf3c773ffa9ffcf98bc595d77cfdfaba85
dlfedie <49173142+dlfedie@users.noreply.github.com>
2022-12-06 15:43:30 -0600

    day4 part1 done

--- day4/index.js
+++ day4/index.js
@@ -1,20 +1,32 @@
-const input = require('./testInput');
+const input = require('./day4Input');
 
 const dayFourPartOne = () => { 
 
   const mainInput = input.pairs.split('\n');
 
   const checkForOverlap = (assignmentPairs) => {
-    console.log(assignmentPairs);
+    // console.log(assignmentPairs);
+    let rangeFullyCovered = 0;
 
     for (let pair of assignmentPairs) {
       // split on the comma, sort also so lowest starting is on the left
-      let firstSplit = pair.split(',').sort();
-      console.log(firstSplit)
-      let lowFirst = Number(firstSplit[0].split('-')[0]);
-      let highFirst = Number(firstSplit[0].split('-')[1]);
-      console.log('low: ', lowFirst, ' high: ', highFirst);
+      let pairSplit = pair.split(',').sort();
+      // console.log(pairSplit)
+      let lowFirst = Number(pairSplit[0].split('-')[0]);
+      let highFirst = Number(pairSplit[0].split('-')[1]);
+      // console.log('low first: ', lowFirst, ' high first: ', highFirst);
+      let lowSecond = Number(pairSplit[1].split('-')[0]);
+      let highSecond = Number(pairSplit[1].split('-')[1]);
+      // console.log('low sec: ', lowSecond, ' high sec: ', highSecond);
+      // check if either range of #s encompasses the other
+      if (lowFirst <= lowSecond && highFirst >= highSecond) {
+        rangeFullyCovered++;
+      } else if (lowSecond <= lowFirst && highSecond >= highFirst) {
+        rangeFullyCovered++;
+      }
+
     }
+    console.log('pairs where one covers another: ', rangeFullyCovered);
   }
 
   checkForOverlap(mainInput);
