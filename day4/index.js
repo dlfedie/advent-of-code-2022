f97c566ad4cb87ed367feebe12f82ececbcab72f
dlfedie <49173142+dlfedie@users.noreply.github.com>
2022-12-06 16:11:43 -0600

    day4 part2 done. thx mdn

--- day4/index.js
+++ day4/index.js
@@ -4,7 +4,7 @@ const dayFourPartOne = () => {
 
   const mainInput = input.pairs.split('\n');
 
-  const checkForOverlap = (assignmentPairs) => {
+  const checkForFullOverlap = (assignmentPairs) => {
     // console.log(assignmentPairs);
     let rangeFullyCovered = 0;
 
@@ -26,11 +26,50 @@ const dayFourPartOne = () => {
       }
 
     }
-    console.log('pairs where one covers another: ', rangeFullyCovered);
+    console.log('pairs where one fully covers another: ', rangeFullyCovered);
   }
 
-  checkForOverlap(mainInput);
+  checkForFullOverlap(mainInput);
 }
 
 
+const dayFourPartTwo = () => { 
+
+  const mainInput = input.pairs.split('\n');
+
+  const checkForAnyOverlap = (assignmentPairs) => {
+    // console.log(assignmentPairs);
+    let rangeHasCoverage = 0;
+
+    for (let pair of assignmentPairs) {
+      // split on the comma, sort also so lowest starting is on the left
+      let pairSplit = pair.split(',').sort();
+      // console.log(pairSplit)
+      let lowFirst = Number(pairSplit[0].split('-')[0]);
+      let highFirst = Number(pairSplit[0].split('-')[1]);
+      // console.log('low first: ', lowFirst, ' high first: ', highFirst);
+      let lowSecond = Number(pairSplit[1].split('-')[0]);
+      let highSecond = Number(pairSplit[1].split('-')[1]);
+      let firstRange = range(lowFirst, highFirst, 1);
+      let secondRange = range(lowSecond, highSecond, 1);
+      // console.log(firstRange);
+      const intersection = firstRange.filter(num => secondRange.includes(num));
+      // console.log('intersect: ', intersection);
+      if (intersection.length > 0) {
+        rangeHasCoverage++;
+      }
+      
+
+    }
+    console.log('pairs where any coverage: ', rangeHasCoverage);
+  }
+
+  // unabashedly copied straight out of MDN: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/from#sequence_generator_range
+  const range = (start, stop, step) =>
+    Array.from({ length: (stop - start) / step + 1 }, (_, i) => start + i * step);
+
+  checkForAnyOverlap(mainInput);
+}
+
 dayFourPartOne();
+dayFourPartTwo();
