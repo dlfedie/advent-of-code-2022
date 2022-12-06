87d36e654a1110bbaa4a049b70d4a233f5d88987
Dustin Fedie <dlfedie@gmail.com>
2022-12-05 21:35:15 -0600

    starting day4

--- day4/index.js
+++ day4/index.js
@@ -2,8 +2,22 @@ const input = require('./testInput');
 
 const dayFourPartOne = () => { 
 
-  const mainInput = input.rucksacks.split('\n');
+  const mainInput = input.pairs.split('\n');
 
+  const checkForOverlap = (assignmentPairs) => {
+    console.log(assignmentPairs);
+
+    for (let pair of assignmentPairs) {
+      // split on the comma, sort also so lowest starting is on the left
+      let firstSplit = pair.split(',').sort();
+      console.log(firstSplit)
+      let lowFirst = Number(firstSplit[0].split('-')[0]);
+      let highFirst = Number(firstSplit[0].split('-')[1]);
+      console.log('low: ', lowFirst, ' high: ', highFirst);
+    }
+  }
+
+  checkForOverlap(mainInput);
 }
 
 
--- day4/testInput.js
+++ day4/testInput.js
@@ -1,10 +1,10 @@
-const rucksacks = 
-`vJrwpWtwJgWrhcsFMMfFFhFp
-jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
-PmmdzqPrVvPwwTWBwg
-wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
-ttgJtRGJQctTZtZT
-CrZsJsPPZsGzwwsLwLmpwMDw`;
+const pairs = 
+`2-4,6-8
+2-3,4-5
+5-7,7-9
+2-8,3-7
+6-6,4-6
+2-6,4-8`;
 
 
-module.exports = { rucksacks };
+module.exports = { pairs };
